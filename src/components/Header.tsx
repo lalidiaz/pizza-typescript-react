40dc2eec2f4386f24cf76441d4a66c59d368ab0d
Laura diaz <lauradiaz@Lauras-MacBook-Air.local>
2021-03-25 18:35:00 +0400

    working on header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,33 +3,49 @@ import menu from "../data/menu.json";
 import { Link } from "react-router-dom";
 import { IoPizzaOutline } from "react-icons/io5";
 import { Rotate as Hamburger } from "hamburger-react";
+import { motion } from "framer-motion";
 
 const Header = () => {
   const [isOpen, setOpen] = useState(false);
+
+  const menuVariants = {
+    opened: {
+      top: "-100vh",
+      backgroundColor: "blue",
+    },
+    closed: {
+      top: 0,
+    },
+  };
   return (
     <header>
-      <div className="header-menu-options">
-        <Link to="/">
-          <IoPizzaOutline size={40} color="white" />
-        </Link>
-        <div className="header-burger">
-          <Hamburger toggled={isOpen} toggle={setOpen} />
-        </div>
-        <div className="header-menu-content">
-          <ul>
-            {menu.map((item) => {
-              const { id, url, name } = item;
-              return (
-                <li className="header-link-menu">
-                  <Link to={url} key={id}>
-                    {name}
-                  </Link>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
+      <div className="header-burger">
+        <Hamburger toggled={isOpen} toggle={setOpen} />
       </div>
+      <motion.ul
+        initial={false}
+        animate={isOpen ? "opened" : "closed"}
+        variants={menuVariants}
+        className="header-menu-content"
+      >
+        <div className="header-icon">
+          <li>
+            <Link to="/">
+              <IoPizzaOutline size={40} color="black" />
+            </Link>
+          </li>
+        </div>
+        {menu.map((item) => {
+          const { id, url, name } = item;
+          return (
+            <li className="header-link-menu">
+              <Link to={url} key={id}>
+                {name}
+              </Link>
+            </li>
+          );
+        })}
+      </motion.ul>
     </header>
   );
 };
